794f8ee147be8e36b5744cd4c3c1cb767fddc3ef
mtalhaergun <mtalhaergun24@gmail.com>
2023-03-22 04:41:33 +0300

    RecyclerView Character limit

--- app/src/main/java/com/example/rickandmortyapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/rickandmortyapp/ui/home/HomeFragment.kt
@@ -1,9 +1,13 @@
 package com.example.rickandmortyapp.ui.home
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,6 +18,8 @@ import androidx.paging.PagingData
 import androidx.paging.filter
 import androidx.paging.insertSeparators
 import androidx.paging.map
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmortyapp.base.BaseFragment
 import com.example.rickandmortyapp.databinding.FragmentHomeBinding
 import com.example.rickandmortyapp.model.character.CharacterResponseItem
@@ -28,15 +34,21 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-    private var firstLocation : List<String> = emptyList()
-    private var firstOpen : Boolean = true
+
     private lateinit var adapterLocation: LocationRecyclerAdapter
+    private lateinit var adapterCharacter : CharacterRecyclerAdapter
     private lateinit var binding : FragmentHomeBinding
     private val viewModel by viewModels<HomeViewModel>()
+    private var firstOpen : Boolean = true
+    private var characterPagingList = arrayListOf<CharacterResponseItem>()
+    private var tempList = arrayListOf<CharacterResponseItem>()
+    var page = 1
+    var isLoading = false
+    val limit = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        viewModel.getLocation()
+
 
     }
 
@@ -59,16 +71,46 @@ class HomeFragment : Fragment() {
     fun observeEvents() {
 
         viewModel.multipleCharacterResponse.observe(viewLifecycleOwner, Observer {
-            val adapter = CharacterRecyclerAdapter(object : CharacterClickListener{
-                override fun onCharacterClick(character: CharacterResponseItem) {
-                    val navigation = HomeFragmentDirections.actionHomeFragmentToDetailFragment(character)
-                    Navigation.findNavController(requireView()).navigate(navigation)
+            isLoading = true
+            binding.progressBarCharacter.visibility = View.VISIBLE
+            if(binding.characterRv.adapter == null){
+                binding.characterRv.adapter = adapterCharacter
+            }
+
+                it?.let {
+                    var start = (page-1)*limit
+                    var end = (page*(limit))-1
+
+                    for(i in start..end){
+                        if(i < it.lastIndex+1){
+                            characterPagingList.add(it[i])
+                            println(characterPagingList[i].name)
+                        }
+                    }
+                    adapterCharacter.setCharacters(characterPagingList)
+                    println(it.lastIndex)
+                    tempList = it as ArrayList<CharacterResponseItem>
+                }
+
+                isLoading = false
+                binding.progressBarCharacter.visibility = View.GONE
+
+            binding.characterRv.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+
+                    val visibleItemCount = (binding.characterRv.layoutManager as? LinearLayoutManager)?.childCount!!
+                    val pastVisibleItem = (binding.characterRv.layoutManager as? LinearLayoutManager)?.findFirstCompletelyVisibleItemPosition()!!
+                    val total = adapterCharacter.itemCount
+
+                    if(!isLoading){
+                        if((visibleItemCount + pastVisibleItem) > total){
+                            page++
+                            viewModel.multipleCharacterResponse.value = tempList
+                        }
+                    }
+                    super.onScrolled(recyclerView, dx, dy)
                 }
             })
-            binding.characterRv.adapter = adapter
-            it?.let {
-                adapter.setCharacters(it as ArrayList<CharacterResponseItem>)
-            }
         })
 
         viewModel.error.observe(viewLifecycleOwner, Observer {
@@ -89,25 +131,28 @@ class HomeFragment : Fragment() {
     private fun createRv(){
         adapterLocation = LocationRecyclerAdapter(object : LocationClickListener{
             override fun onLocationClick(location: Result) {
-                    val characterIds = viewModel.selectIds(location.residents)
-                    if (characterIds != null) {
-                        viewModel.getMultipleCharacters(characterIds,location.residents.size)
-                    }
-                    else{
-                        binding.characterRv.adapter = null
-                    }
+                val characterIds = viewModel.selectIds(location.residents)
+                if (characterIds != null) {
+                    binding.characterRv.adapter = null
+                    characterPagingList.clear()
+                    page = 1
+                    viewModel.getMultipleCharacters(characterIds,location.residents.size)
                 }
+                else{
+                    binding.characterRv.adapter = null
+                }
+            }
 
         })
         binding.locationRv.adapter = adapterLocation
+        adapterCharacter = CharacterRecyclerAdapter(object : CharacterClickListener{
+            override fun onCharacterClick(character: CharacterResponseItem) {
+                val navigation = HomeFragmentDirections.actionHomeFragmentToDetailFragment(character)
+                Navigation.findNavController(requireView()).navigate(navigation)
+            }
+        })
+        binding.characterRv.adapter = adapterCharacter
         viewModel.getFirstLocation()
-//        if (firstLocation == null) {
-//            firstLocation = it.results?.get(0)?.residents
-//            val ids = viewModel.selectIds(firstLocation)
-//            if (ids != null) {
-//                viewModel.getMultipleCharacters(ids,firstLocation!!.size)
-//            }
-//        }
     }
 
     private fun loadData() {
